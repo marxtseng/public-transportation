f71dd6d37732304b5d90875a491dadbee276adc1
Marx Tseng <marx.zeng@gmail.com>
2016-09-22 20:49:41 +0800

    first commit

---
+++ demo/sw.js
@@ -0,0 +1,21 @@
+'use strict';
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('public-transportation').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/data.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) return response;
+            return fetch(event.request);
+        })
+    );
+});
---
+++ dist/sw.js
@@ -0,0 +1,23 @@
+'use strict';
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('public-transportation').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/public-transportation.html',
+                '/public-transportation.js',
+                '/data.json',
+                '/webcomponents-lite.min.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
